49cf1d578bd63626e1ff6cbbd1a356f35df0f011
Elad Ben-Israel <elad.benisrael@gmail.com>
2011-05-06 23:47:26 +0300

    Initial Commit

---
+++ telobike/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  telobike
+//
+//  Created by eladb on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ telobike/telobikeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  telobikeAppDelegate.h
+//  telobike
+//
+//  Created by eladb on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface telobikeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
