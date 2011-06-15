b815a0a99e09db1f22c8302c110d95be89cc4c17
Elad Ben-Israel <elad.benisrael@gmail.com>
2011-06-15 22:38:15 +0300

    add culture-specific urls to city

--- backend/src/cities_endpoint.py
+++ backend/src/cities_endpoint.py
@@ -19,6 +19,7 @@ class City(db.Model):
     mail_tags = db.StringProperty()
     city_center = db.GeoPtProperty()
     disclaimer = db.StringProperty()
+    info_url_he = db.LinkProperty()
     info_url = db.LinkProperty()
     
     @classmethod
@@ -26,14 +27,19 @@ class City(db.Model):
         existing = City.get_by_key_name('tlv')
         if not existing:
             tlv = City.get_or_insert(key_name = 'tlv')
-            tlv.service_name = 'Tel-o-Fun.HE'
+            tlv.service_name = 'Tel-o-Fun'
             tlv.service_name_en = 'Tel-o-Fun'
-            tlv.city_name = 'Tel-Aviv.HE'
+            tlv.city_name = 'Tel-Aviv'
             tlv.city_name_en = 'Tel-Aviv'
             tlv.mail = 'info@fsm-tlv.co.il'
             tlv.mail_tags = '#malfunction'
-            tlv.info_url = 'http://www.tel-o-fun.co.il'
+            tlv.info_url = 'http://telobike.citylifeapps.com/static/en/tlv.html'
+            tlv.info_url_he = 'http://telobike.citylifeapps.com/static/he/tlv.html'
             tlv.put()
+            
+        if not existing.info_url_he:
+            existing.info_url_he = 'http://telobike.citylifeapps.com/static/he/tlv.html'
+            existing.put()
     
 class CityEndpoint(restapp.Endpoint):
     root_url = '/cities'
