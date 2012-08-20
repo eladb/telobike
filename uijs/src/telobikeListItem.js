1cfb2a46d3095429ef292feb9ad2547b383e9386
unknown <ramgreenberg@hotmail.com>
2012-08-20 20:57:55 +0300

    add searchBar to telobike list view

--- uijs/src/telobikeListItem.js
+++ uijs/src/telobikeListItem.js
@@ -25,6 +25,9 @@ module.exports = function(boxItem) {
   images['assets/img/list_full.png'] = util.loadimage('assets/img/list_full.png');
   images['assets/img/list_hfull.png'] = util.loadimage('assets/img/list_hfull.png');
   images['assets/img/list_okay.png'] = util.loadimage('assets/img/list_okay.png');
+  images['assets/img/MyLocation.png'] = util.loadimage('assets/img/MyLocation.png');
+  images['assets/img/white_arrow.png'] = util.loadimage('assets/img/white_arrow.png');
+  images['assets/img/arrow.png'] = util.loadimage('assets/img/arrow.png');
 
   var img = image({
     image: bind(img, 'image', function(){ 
@@ -32,83 +35,86 @@ module.exports = function(boxItem) {
     }),
     x:10,
     y:10,
-    width:47,
-    height:47,
+    width:50,
+    height:50,
   });
 
   var textLocationStart = 17;
-    
+
+
   var location = label({
-    text:bind(location, 'text', function() { return boxItem.data.name_en; }),
-    x:67,
-    y:textLocationStart,
-    size:14,
-    width:boxItem.width - 45 - 67 - 5,
-    height:20,
-    bold:true,
+    text: bind(location, 'text', function() { return boxItem.data.name_en; }),
+    x: 67,
+    y: textLocationStart,
+    size: 14,
+    width: boxItem.width - 45 - 67 - 5,
+    height: bind(location, 'height', function(){
+      return boxItem.data.name_en != 'Current Position' ?  20 : 40;
+    }),
+    bold: bind(location, 'bold', function(){ return (boxItem.data.name_en != 'Current Position'); }),
     align: 'left',
   });
 
   var bikeStatus = label({
-    text:bind(bikeStatus, 'text', function(){return "bikes: " +  boxItem.data.available_bike;}),
-    color:bind(bikeStatus, 'color', function(){
+    text: bind(bikeStatus, 'text', function(){ return "bikes: " +  boxItem.data.available_bike;}),
+    color: bind(bikeStatus, 'color', function(){
       if(boxItem.data.status === 'empty') return 'red';
       if(boxItem.data.status === 'hempty') return 'orange';
       return 'black';
     }),
-    x:67,
-    y:textLocationStart + 20,
-    size:14,
-    width:100,
-    height:20,
+    x: 67,
+    y: textLocationStart + 20,
+    size: 14,
+    width: 100,
+    height: 20,
     align: 'left',
+    visible: bind(function(){ return boxItem.data.name_en != 'Current Position'; }),
   });
 
   var parkingStatus = label({
-    text:bind(parkingStatus, 'text', function(){return "parking: " +  boxItem.data.available_spaces; }),
-    color:bind(parkingStatus, 'color', function(){
+    text: bind(parkingStatus, 'text', function(){return "parking: " +  boxItem.data.available_spaces; }),
+    color: bind(parkingStatus, 'color', function(){
       if(boxItem.data.status === 'full') return 'red';
       if(boxItem.data.status === 'hfull') return 'orange';
       return 'black';
     }),
-    x:167,
-    y:textLocationStart + 20,
-    size:14,
-    width:boxItem.width - 45 - 67,
-    height:20,
+    x: 167,
+    y: textLocationStart + 20,
+    size: 14,
+    width: boxItem.width - 45 - 67,
+    height: 20,
     align: 'left',
+    visible: bind(function(){ return boxItem.data.name_en != 'Current Position'; }),
   });
 
-  var whiteArrowImage = util.loadimage('assets/img/white_arrow.png');
-  var grayArrowImage = util.loadimage('assets/img/arrow.png');
-
   var arrow = image({
-    image: bind(arrow, 'image', function(){ return (boxItem.data.select || boxItem.highlight) ? whiteArrowImage : grayArrowImage;}),
+    image: bind(arrow, 'image', function(){ return (boxItem.data.select || boxItem.highlight) ? images['assets/img/white_arrow.png'] : images['assets/img/arrow.png'];}),
     x: boxItem.width - 45,
-    y:20,
-    width:20,
-    height:25,
+    y: 20,
+    width: 20,
+    height: 25,
   });
 
   var distance = label({
-    text:bind(distance, 'text', function(){return (boxItem.data.distance) ? 
+    text: bind(distance, 'text', function(){
+      return (boxItem.data.distance) ? 
       ((boxItem.data.distance >= 1000) ? (boxItem.data.distance/1000).toFixed(1) +' km' :  boxItem.data.distance +' m') : 
       'undefined'; }),
-    x:boxItem.width - 50,
-    y:textLocationStart + 30,
-    size:10,
-    width:50,
-    height:15,
-    color:'gray',
+    x: boxItem.width - 50,
+    y: textLocationStart + 30,
+    size: 10,
+    width: 50,
+    height: 15,
+    color: 'gray',
     align: 'left',
   });
 
   var seperator = rect({
-    x:0,
-    y:boxItem.height - 1,
-    width:boxItem.width,
-    height:1,
-    color:'gray',
+    x: 0,
+    y: boxItem.height - 1,
+    width: boxItem.width,
+    height: 1,
+    color: 'gray',
   });
 
   boxItem.add(backround);
@@ -119,5 +125,6 @@ module.exports = function(boxItem) {
   boxItem.add(arrow);
   boxItem.add(distance);
   boxItem.add(seperator);
+
 };
   
--- uijs/src/telobikeListView.js
+++ uijs/src/telobikeListView.js
@@ -1,28 +1,48 @@
 var uijs = require('uijs');
 var scroller = uijs.scroller;
+var util = uijs.util;
 var box = uijs.box;
 var controls = require('uijs-controls');
 var telobikeListItem = require('./telobikeListItem');
 var listview = controls.listview;
 var bind = uijs.bind;
+var searchBar = controls.searchBar;
 
 function stripes() {
-  var obj = listview({
+    
+  var obj = listview({   
     items:[],
     onBindBoxItem: telobikeListItem,
     itemHeight:68,
+    filterCondition: function(data,value){
+      return (data.name_en.toLowerCase().indexOf(value.toLowerCase()) != -1); 
+    },
+    searchBar:searchBar({
+      height:40,
+      backgroundImage:util.loadimage('assets/img/searchBar.png'),
+    }),
   });
 
   var model = require('./model').createModel();
 
   model.on('update', function() {
-    obj.items = model.stations.sort(function(a,b){
+    var stations = model.stations.sort(function(a,b){
       if(!a.distance || !b.distance)
       {
         return 0;
       }
       return (parseInt(a.distance) - parseInt(b.distance));
     });
+    
+    stations.unshift({
+      city: 'tlv',
+      name_en: 'Current Position',
+      status: 'okay',
+      list_image: 'assets/img/MyLocation.png',
+      distance: 0, 
+    });
+
+    obj.items = stations;
   });
   
   obj.on('click',function(data){
