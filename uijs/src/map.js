6da32501742e5e693c6c12517598197799e8a5ad
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-08-28 17:32:55 +0300

    Create nativebox

--- uijs/src/map.js
+++ uijs/src/map.js
@@ -1,10 +1,11 @@
 var uijs = require('uijs');
 var box = uijs.box;
 var defaults = uijs.util.defaults;
-var nativeobj = require('./nativeobj');
+var nativebox = require('./nativebox');
 
 module.exports = function(options) {
-  var obj = box(defaults(options, {
+  var obj = nativebox(defaults(options, {
+    type: 'UIJSMap',
     markers: [], // a marker contains at least { location: [ lat, long ] }
     region: {
       center: [ 0, 0 ],
@@ -15,71 +16,24 @@ module.exports = function(options) {
       track: true,
       heading: false
     },
-    invalidators: [ 'bounds' ],
   }));
 
-  obj.bind('bounds', function() { 
-    return this.x + ',' + this.y + '-' + this.width + 'x' + this.height; 
-  });
-
-  var native_map = nativeobj('UIJSMap', obj._id, {});
-
-  // forward all events from `native_map` to `obj` (like!)
-  native_map.forward(obj);
-
-  // native_map.mock = true;
-
-  var subscribed = false;
-
-  obj.watch('bounds', function() {
-    native_map.call('move', {
-      x: this.x,
-      y: this.y,
-      width: this.width,
-      height: this.height
+  obj.on('init', function(native_map) {
+    obj.watch('markers', function() {
+      console.log('markers changed');
+      native_map.call('set_markers', this.markers);
     });
 
-    // only subscribe *after* we did some positioning
-    if (!subscribed) {
-      
-      obj.watch('markers', function() {
-        console.log('markers changed');
-        native_map.call('set_markers', this.markers);
-      });
+    obj.watch('region', function() {
+      console.log('region changed');
+      native_map.call('set_region', this.region);
+    });
 
-      obj.watch('region', function() {
-        console.log('region changed');
-        native_map.call('set_region', this.region);
-      });
-
-      obj.watch('userLocation', function() {
-        console.log('userLocation changed');
-        native_map.call('set_user_location', this.userLocation);
-      });      
-
-      subscribed = true;
-    }
+    obj.watch('userLocation', function() {
+      console.log('userLocation changed');
+      native_map.call('set_user_location', this.userLocation);
+    });      
   });
 
-  obj.ondraw = function(ctx) {
-    if (!native_map.mock) {
-      ctx.clearRect(0, 0, this.width, this.height);
-    }
-    else {
-      ctx.fillStyle = 'white';
-      ctx.fillRect(0, 0, this.width, this.height);
-      ctx.strokeStyle = 'red';
-      ctx.beginPath();
-      ctx.moveTo(0, 0);
-      ctx.lineTo(this.width, this.height);
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.moveTo(this.width, 0);
-      ctx.lineTo(0, this.height);
-      ctx.stroke();
-      ctx.strokeRect(0, 0, this.width, this.height);
-    }
-  };
-
   return obj;
-}
+};
