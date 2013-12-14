32a6ed2f02520c0aa931ea0f404936a2e18ece13
Elad Ben-Israel <elad.benisrael@gmail.com>
2013-12-14 18:25:28 +0200

    Add Appirater

--- ios/telobike/telobike/TBAppDelegate.m
+++ ios/telobike/telobike/TBAppDelegate.m
@@ -6,12 +6,27 @@
 //  Copyright (c) 2013 Elad Ben-Israel. All rights reserved.
 //
 
+#import <Appirater.h>
 #import "TBAppDelegate.h"
+
 
 @implementation TBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [Appirater setAppId:@"436915919"];
+    [Appirater setDaysUntilPrompt:3];
+    [Appirater setUsesUntilPrompt:5];
+    [Appirater setSignificantEventsUntilPrompt:-1];
+    [Appirater setTimeBeforeReminding:2];
+    [Appirater setDebug:YES];
+    
+    [Appirater appLaunched:YES];
     return YES;
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [Appirater appEnteredForeground:YES];
 }
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
