4500cad54df029fec3c4cf80ba826d4e5b733c7e
Elad Ben-Israel <elad.benisrael@gmail.com>
2013-12-14 19:00:46 +0200

    Alert on location services disabled

--- ios/telobike/telobike/TBAppDelegate.m
+++ ios/telobike/telobike/TBAppDelegate.m
@@ -9,18 +9,24 @@
 #import <Appirater.h>
 #import "TBAppDelegate.h"
 
+@interface TBAppDelegate () <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager* locationManager;
+
+@end
 
 @implementation TBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     [Appirater setAppId:@"436915919"];
     [Appirater setDaysUntilPrompt:3];
     [Appirater setUsesUntilPrompt:5];
     [Appirater setSignificantEventsUntilPrompt:-1];
     [Appirater setTimeBeforeReminding:2];
-    [Appirater setDebug:YES];
-    
+    [Appirater setDebug:NO];
+
+    [self alertOnLocationServicesDisabled];
+
     [Appirater appLaunched:YES];
     return YES;
 }
@@ -31,6 +37,27 @@
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
     [[NSNotificationCenter defaultCenter] postNotificationName:@"didReceiveLocalNotification" object:notification];
+}
+
+- (void)alertOnLocationServicesDisabled {
+    if (!self.locationManager) {
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+    }
+    
+    [self.locationManager startUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    [manager stopUpdatingLocation];
+    
+    if (error.code == kCLErrorDenied) {
+        [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Location Services Disabled for Telobike", Nil)
+                                    message:NSLocalizedString(@"Go to the Settings app and under Privacy -> Location Services, enable Telobike", nil)
+                                   delegate:nil
+                          cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                          otherButtonTitles:nil] show];
+    }
 }
 
 @end
