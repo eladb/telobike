277b13600c26c434f57860134b3a1f215def4f03
Elad Ben-Israel <elad.benisrael@gmail.com>
2013-12-29 13:16:48 +0200

    update disclaimer

--- new-backend/server.js
+++ new-backend/server.js
@@ -104,7 +104,7 @@ server.get('/cities/tlv', function(req, res) {
   city.city_center = "32.0664,34.7779";
   city.city_name = "תל-אביב יפו";
   city['city_name.en'] = "Tel-Aviv";
-  city.disclaimer = "Horray! The recent issues with the Tel-o-Fun database have been resolved. Enjoy cycling! Nir and Elad";
+  city.disclaimer = "Welcome to Telobike. We created this app as a public service for all the awesome people of Tel-Aviv. Let us know what you think! Nir and Elad.";
   city.info_url = "http://telobike.citylifeapps.com/static/en/tlv.html";
   city.info_url_he = "http://telobike.citylifeapps.com/static/he/tlv.html";
   city.last_update = "2011-06-15 18:47:50.982111";
