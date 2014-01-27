1d4be9fa058085574ca5a2ab94878ec9575b984b
Elad Ben-Israel <elad.benisrael@gmail.com>
2014-01-27 18:43:58 +0200

    Default routing by "walking"

--- ios/telobike/telobike/TBGoogleMapsRouting.m
+++ ios/telobike/telobike/TBGoogleMapsRouting.m
@@ -13,39 +13,14 @@
 + (BOOL)routeFromAddress:(NSString*)startAddress toAddress:(NSString*)destinationAddress {
 	NSMutableString *googleMapsURLString = [NSMutableString stringWithFormat:@"%@?", @"comgooglemaps://"];
 	NSMutableArray *components = [NSMutableArray array];
-	
-//	if (self.queryString) {
-//		[components addObject:[self parameterStringWithKey:kJNJGoogleMapsQueryKeyQuery value:self.queryString]];
-//	}
-	
+		
 	if (startAddress && destinationAddress) {
 		[components addObject:[[self class] parameterStringWithKey:@"saddr" value:startAddress]];
 		[components addObject:[[self class] parameterStringWithKey:@"daddr" value:destinationAddress]];
 	}
     
-//	if (self.latitude && self.longitude) {
-//		NSString *position = [NSString stringWithFormat:@"%@,%@", [self.latitude stringValue], [self.longitude stringValue]];
-//		[components addObject:[self parameterStringWithKey:kJNJGoogleMapsQueryKeyCenter value:position]];
-//	}
+    [components addObject:[[self class] parameterStringWithKey:@"directionsmode" value:@"walking"]];
     
-//	if (self.zoomLevel > 0) {
-//		NSString *zoomLevel = [NSString stringWithFormat:@"%i", self.zoomLevel];
-//		[components addObject:[self parameterStringWithKey:kJNJGoogleMapsQueryKeyZoom value:zoomLevel]];
-//	}
-	
-//	if (self.mapMode) {
-//		[components addObject:[self parameterStringWithKey:kJNJGoogleMapsQueryKeyMapMode value:self.mapMode]];
-//	}
-	
-//	if (self.directionMode) {
-//		[components addObject:[self parameterStringWithKey:kJNJGoogleMapsQueryKeyDirectionMode value:self.directionMode]];
-//	}
-	
-//	if ([self.viewTypes count] > 0) {
-//		NSString *viewTypes = [self.viewTypes componentsJoinedByString:@","];
-//		[components addObject:[self parameterStringWithKey:kJNJGoogleMapsQueryKeyViews value:viewTypes]];
-//	}
-	
 	NSString *componentsString = [components componentsJoinedByString:@"&"];
     if ([componentsString length] > 0) {
         [googleMapsURLString appendFormat:@"&%@", componentsString];
