1c27c385ff1f8306a3062b96533ec7abd2374af3
Elad Ben-Israel <elad.benisrael@gmail.com>
2014-12-22 21:57:40 +0000

    Do not use iOS8 APIs for alerts

--- ios/telobike/telobike/TBAlerts.swift
+++ ios/telobike/telobike/TBAlerts.swift
@@ -9,15 +9,53 @@
 import UIKit
 
 class TBAlerts {
-    class func showAlertFromViewController(viewController: UIViewController, title: String, message: String? = nil, dismissed: () -> () = {}) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .Default, handler: nil))
-        viewController.presentViewController(alert, animated: true, completion: dismissed)
+    class func showAlertFromViewController(viewController: UIViewController, title: String, message: String? = nil, dismissed: (() -> ())? = nil) {
+        UIAlertView(
+            title: title,
+            message: message,
+            delegate: AlertDelegate(callback: dismissed),
+            cancelButtonTitle: NSLocalizedString("OK", comment: "")).show()
     }
     
     class func showAlert(#title: String, message: String? = nil) {
         if let vc = UIApplication.sharedApplication().keyWindow?.rootViewController {
             self.showAlertFromViewController(vc, title: title, message: message)
+        }
+    }
+}
+
+private class AlertDelegate: NSObject, UIAlertViewDelegate {
+    private let callback: (() -> ())?
+    init(callback: (() -> ())?) {
+        self.callback = callback
+        super.init()
+        DelegateManager.ref(self)
+    }
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        self.callback?()
+        DelegateManager.unref(self)
+    }
+}
+
+private struct DelegateManager {
+    private static var delegates = [AlertDelegate]()
+    
+    static func ref(delegate: AlertDelegate) {
+        delegates.append(delegate)
+    }
+    
+    static func unref(delegtae: AlertDelegate) {
+        var idx: Int?
+        
+        for i in 0..<delegates.count {
+            if delegates[i] === delegtae {
+                idx = i
+                break
+            }
+        }
+        
+        if let idx = idx {
+            delegates.removeAtIndex(idx)
         }
     }
 }
