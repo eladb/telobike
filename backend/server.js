12d5a64b2f120b6d990fedb28d535753a4d016e7
Elad Ben-Israel <elad.benisrael@gmail.com>
2015-11-29 22:24:53 +0000

    With status

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ var csvdb = require('csvdb');
 var telofun_api = require('./lib/telofun-api');
 var telofun_mapper = require('./lib/telofun-mapper');
 var AWS = require('aws-sdk');
+var async = require('async');
 
 AWS.config.region = 'eu-west-1';
 
@@ -15,31 +16,42 @@ var s3bucket = new AWS.S3({ params: { Bucket: 'telobike' } });
 console.log('telobike server is running...');
 
 var overrides_url = 'https://docs.google.com/spreadsheets/d/1qjbQfj2vDWc569PIXJ-i8-2uLQk3KC1P4mz3bpGUJxI/pub?output=csv';
-var overrides_db = csvdb(overrides_url, { autofetch: 30 * 1000 }); // refresh overrides every 30s
-
-setTimeout(function() {
-  console.log(overrides_db.entries);
-}, 5000);
+var s3_url_prefix = 'https://s3-eu-west-1.amazonaws.com/telobike';
 
 server.use(express.logger());
 server.use(express.methodOverride());
 server.use(cors());
 server.use(express.favicon(path.join(__dirname, 'public/img/favicon.png')));
 
+var last_read_status = {
+  time: 'never',
+  api: 'unknown',
+  overrides: 'unknown',
+  s3: 'unknown',
+};
 
 var stations = {};
 
-function read_stations(callback) {
+function render_stations(callback) {
   callback = callback || function() {};
   logule.trace('reading station information from tel-o-fun');
 
+  last_read_status = {
+    time: new Date(),
+    api: 'pending',
+    overrides: 'pending',
+    s3: 'pending',
+  };
+
   return telofun_api(function(err, updated_stations) {
     if (err || !updated_stations) {
+      last_read_status.api = 'Error: ' + err.message;
       console.error('error: unable to read telofun stations:', err);
       return callback(err);
     }
 
     console.log(updated_stations.length + ' stations retrieved');
+    last_read_status.api = 'Loaded ' + updated_stations.length.toString() + ' stations';
 
     // map stations from tel-o-fun protocol to tel-o-bike protocol
     var mapped_stations = updated_stations.map(telofun_mapper);
@@ -52,20 +64,29 @@ function read_stations(callback) {
       }
     });
 
-    // merge overrides
-    merge_overrides(mapped_stations);
-
-    console.log(JSON.stringify(mapped_stations, true, 2));
-
-    // write stations to S3
-    upload_to_s3(mapped_stations, function(err) {
+    return csvdb(overrides_url, function(err, all_overrides) {
       if (err) {
-        console.error('ERROR: upload to s3 failed:', err);
+        last_read_status.overrides = 'Error: ' + err.message;
       }
-      console.log('Upladed to S3');
-    });
+      else {
+        last_read_status.overrides = 'Success. Loaded ' + Object.keys(all_overrides).length.toString() + ' overrides';
 
-    return callback(null, mapped_stations);
+        // merge overrides
+        merge_overrides(mapped_stations, all_overrides);
+      }
+
+      // write stations to S3
+      upload_to_s3(mapped_stations, function(err) {
+        if (err) {
+          console.error('ERROR: upload to s3 failed:', err);
+          last_read_status.s3 = 'Error: ' + err.message;
+        }
+        console.log('Uploaded to S3');
+        last_read_status.s3 = 'Uploaded';
+      });
+
+      return callback(null, mapped_stations);
+    });
   });
 }
 
@@ -75,11 +96,11 @@ function upload_to_s3(stations, callback) {
   return s3bucket.upload(params, callback);
 }
 
-function merge_overrides(stations) {
+function merge_overrides(stations, all_overrides) {
   for (var sid in stations) {
     var s = stations[sid];
 
-    var overrides = overrides_db.entries[sid];
+    var overrides = all_overrides;
     if (overrides) {
       console.log('found overrides for', sid);
       for (var k in overrides) {
@@ -92,55 +113,27 @@ function merge_overrides(stations) {
   }
 }
 
-setInterval(read_stations, 30*1000); // update station info every 30 seconds
-read_stations();
+setInterval(render_stations, 30*1000); // update station info every 30 seconds
+render_stations();
 
-setInterval(function() {
-  var mb = Math.round(process.memoryUsage().heapTotal / 1024);
-  logule.trace(mb + ' kb');
-}, 2500);
+function get_tlv_stations(req, res) {
+  return res.redirect(s3_url_prefix + '/tlv/stations.json');
+}
 
-function get_stations(req, res) {
-  if (!stations) {
-    res.writeHead(404);
-    return res.end('no stations information from tel-o-fun');
-  }
-
-  // convert to hash
-  var result = Object.keys(stations).map(function(key) {
-    return stations[key];
-  });
-
-  return res.send(result);
+function get_tlv_city(req, res) {
+  return res.redirect(s3_url_prefix + '/tlv/city.json');
 }
 
 server.get('/', function(req, res) {
-  var host_parts = req.headers.host.split(':')[0].split('.');
-  if (host_parts[0] === 'worldtour') {
-    return res.redirect('http://www.tripcolor.com/user/53139/trip/C027FFEF-C4CF-4A4F-AA13-B8B11D1E4D75')
-  }
-
   return res.redirect('http://itunes.apple.com/us/app/tel-o-bike-tel-aviv-bicycle/id436915919?mt=8');
 });
 
-server.get('/stations', get_stations);
-server.get('/tlv/stations', get_stations);
-server.get('/cities/tlv', function(req, res) {
-  var city = {};
-  city.city = "tlv";
-  city.city_center = "32.0664,34.7779";
-  city.city_name = "תל-אביב יפו";
-  city['city_name.en'] = "Tel-Aviv";
-  // city.disclaimer = "";
-  // city.disclaimer = "Welcome to Telobike. We created this app as a public service for all the awesome people of Tel-Aviv. Let us know what you think! Nir and Elad.";
-  city.info_url = "http://telobike.citylifeapps.com/static/en/tlv.html";
-  city.info_url_he = "http://telobike.citylifeapps.com/static/he/tlv.html";
-  city.last_update = "2011-06-15 18:47:50.982111";
-  city.mail = "info@fsm-tlv.com";
-  city.mail_tags = "This problem was reported via telobike";
-  city.service_name= "תל-אופן";
-  city['service_name.en'] = "Tel-o-Fun";
-  return res.send(city);
+server.get('/stations', get_tlv_stations);
+server.get('/tlv/stations', get_tlv_stations);
+server.get('/cities/tlv', get_tlv_city);
+
+server.get('/status', function(req, res) {
+  res.send(last_read_status);
 });
 
 server.get('/ping', function(req, res) {
@@ -168,6 +161,11 @@ server.post('/_deploy_dskfjh484jk09k', function(req, res) {
   }, backoff_sec * 1000);
   res.end('OK');
 });
+
+setInterval(function() {
+  var mb = Math.round(process.memoryUsage().heapTotal / 1024);
+  logule.trace(mb + ' kb');
+}, 2500);
 
 server.listen(process.env.port || 5000);
 
